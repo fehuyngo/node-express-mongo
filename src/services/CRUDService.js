a8af7b3438f98ca33e73f20abb5ab032682f8374
fehuyngo <fe.huyngo@gmail.com>
2024-08-27 10:09:12 +0700

    .

--- src/services/CRUDService.js
+++ src/services/CRUDService.js
@@ -7,6 +7,15 @@ const getAllUsers = async () => {
     return results;
 }
 
+const getUserById = async (userId) => {
+    let [results, fields] = await connection.query(
+        `SELECT * FROM Users WHERE id = ?`, [userId],
+    );
+    let user = results && results.length > 0 ? results[0] : {};
+    return user;
+}
+
 module.exports = {
-    getAllUsers
+    getAllUsers,
+    getUserById
 }
