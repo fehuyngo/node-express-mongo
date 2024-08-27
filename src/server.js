1765d1e66f69b3f019ae9b337992553fd0d9ce29
fehuyngo <fe.huyngo@gmail.com>
2024-08-27 14:22:43 +0700

    .

--- src/server.js
+++ src/server.js
@@ -20,14 +20,7 @@ configViewEngine(app);
 app.use('/', webRoutes);
 
 //test connection
-
-// simple query
-// connection.query(
-//     'SELECT * FROM Users',
-//     (err, results, fields) => {
-//         console.log(">>> results= ", results);
-//     }
-// );
+connection();
 
 app.listen(port, hostname, () => {
     console.log(`Example app listening on port ${port}`)
