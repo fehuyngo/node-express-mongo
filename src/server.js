8463baf34e8564aa0474a014cb68db808b77129a
fehuyngo <fe.huyngo@gmail.com>
2024-09-09 13:13:58 +0700

    .

--- src/server.js
+++ src/server.js
@@ -3,12 +3,17 @@ const express = require('express');//commonjs
 const configViewEngine = require('./config/viewEngine');
 const webRoutes = require('./routes/web');
 const apiRoutes = require('./routes/api');
+const fileUpload = require('express-fileupload');
+
 const connection = require('./config/database');
 
 // import express from 'express';//es modules
 const app = express(); // app express
 const port = process.env.PORT || 8888; //port
 const hostname = process.env.HOST_NAME;
+
+// config file upload
+app.use(fileUpload());
 
 // config req.body
 app.use(express.json());
